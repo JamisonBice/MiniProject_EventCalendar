b156f2de030f2f267585b4640473c6fa0bf8e41b
Nick <ntambroson@dmacc.edu>
2021-10-16 11:55:38 -0500

    Added EventSort class

---
+++ src/main/java/model/EventSort.java
@@ -0,0 +1,51 @@
+package model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+/**
+ * @author Nick - ntambroson
+ * CIS175 - Fall 2021
+ * Oct 16, 2021
+ */
+@Entity
+public class EventSort {
+	@Id
+	@GeneratedValue
+	private int id;
+	private String eventSortName;
+	public EventSort() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public EventSort(int id, String eventSortName) {
+		super();
+		this.id = id;
+		this.eventSortName = eventSortName;
+	}
+	public EventSort(String eventSortName) {
+		super();
+		this.eventSortName = eventSortName;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getEventSortName() {
+		return eventSortName;
+	}
+	public void setEventSortName(String eventSortName) {
+		this.eventSortName = eventSortName;
+	}
+
+	@Override
+	public String toString() {
+		return "Event Name [id=" + id + ", eventSortName=" + eventSortName + "]";
+	}
+
+}
+
