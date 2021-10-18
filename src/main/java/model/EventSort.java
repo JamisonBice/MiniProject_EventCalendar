f62bbe274404fd4bfb551e55fc8543de455a89bd
JamisonBice <89953910+JamisonBice@users.noreply.github.com>
2021-10-17 23:02:29 -0500

    Update EventSort.java

--- src/main/java/model/EventSort.java
+++ src/main/java/model/EventSort.java
@@ -7,42 +7,46 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 /**
- * @author Nick - ntambroson
- * CIS175 - Fall 2021
- * Oct 16, 2021
+ * @author Nick - ntambroson CIS175 - Fall 2021 Oct 16, 2021
  */
 @Entity
-@Table(name="sort")
+@Table(name = "sort")
 public class EventSort {
 	@Id
 	@GeneratedValue
 	private int id;
-	
-	@Column(name="Sort_Name")
+
+	@Column(name = "Sort_Name")
 	private String eventSortName;
+
 	public EventSort() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
+
 	public EventSort(int id, String eventSortName) {
 		super();
 		this.id = id;
 		this.eventSortName = eventSortName;
 	}
+
 	public EventSort(String eventSortName) {
 		super();
 		this.eventSortName = eventSortName;
 	}
-	
+
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	public String getEventSortName() {
 		return eventSortName;
 	}
+
 	public void setEventSortName(String eventSortName) {
 		this.eventSortName = eventSortName;
 	}
@@ -53,4 +57,3 @@ public class EventSort {
 	}
 
 }
-
