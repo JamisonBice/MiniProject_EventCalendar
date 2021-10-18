71d6d0a77a58fae50c1c173bca153d44499ed755
JamisonBice <89953910+JamisonBice@users.noreply.github.com>
2021-10-17 23:02:11 -0500

    Update EventSortHelper.java

--- src/main/java/controller/EventSortHelper.java
+++ src/main/java/controller/EventSortHelper.java
@@ -2,7 +2,6 @@ package controller;
 
 import java.util.List;
 
-
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.NoResultException;
@@ -12,9 +11,7 @@ import javax.persistence.TypedQuery;
 import model.EventSort;
 
 /**
- * @author Nick - ntambroson
- * CIS175 - Fall 2021
- * Oct 16, 2021
+ * @author Nick - ntambroson CIS175 - Fall 2021 Oct 16, 2021
  */
 public class EventSortHelper {
 
@@ -33,17 +30,21 @@ public class EventSortHelper {
 		List<EventSort> allEventSorts = em.createQuery("SELECT d  FROM EventSort d").getResultList();
 		return allEventSorts;
 	}
-	public EventSort findEventSort(String nameToLookUp) { 
-		EntityManager em = emfactory.createEntityManager(); em.getTransaction().begin(); 
-		TypedQuery<EventSort> typedQuery = em.createQuery("select de  from EventSort de where de.eventSortName = :selectedName", EventSort.class);
-		typedQuery.setParameter("selectedName", nameToLookUp); typedQuery.setMaxResults(1); 
-		EventSort foundEventSort; 
-		try { 
-		foundEventSort = typedQuery.getSingleResult(); 
-		} catch (NoResultException ex) { 
-		foundEventSort = new EventSort(nameToLookUp); 
-		} 
-		em.close(); 
-		return foundEventSort; 
+
+	public EventSort findEventSort(String nameToLookUp) {
+		EntityManager em = emfactory.createEntityManager();
+		em.getTransaction().begin();
+		TypedQuery<EventSort> typedQuery = em
+				.createQuery("select de  from EventSort de where de.eventSortName = :selectedName", EventSort.class);
+		typedQuery.setParameter("selectedName", nameToLookUp);
+		typedQuery.setMaxResults(1);
+		EventSort foundEventSort;
+		try {
+			foundEventSort = typedQuery.getSingleResult();
+		} catch (NoResultException ex) {
+			foundEventSort = new EventSort(nameToLookUp);
 		}
+		em.close();
+		return foundEventSort;
+	}
 }
