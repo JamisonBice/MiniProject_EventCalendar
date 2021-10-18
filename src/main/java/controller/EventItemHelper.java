9f28a3a829c708c3ecd26471e5a523d55342c8f5
JamisonBice <89953910+JamisonBice@users.noreply.github.com>
2021-10-17 23:02:06 -0500

    Update EventItemHelper.java

--- src/main/java/controller/EventItemHelper.java
+++ src/main/java/controller/EventItemHelper.java
@@ -10,46 +10,42 @@ import javax.persistence.TypedQuery;
 
 import model.EventItem;
 
-
 /**
- * @author Nick - ntambroson
- * CIS175 - Fall 2021
- * Oct 14, 2021
+ * @author Nick - ntambroson CIS175 - Fall 2021 Oct 14, 2021
  */
 public class EventItemHelper {
-	
+
 	static EntityManagerFactory emfactory = Persistence.createEntityManagerFactory("MiniProject_EventCalendar");
-	
+
 	public void insertItem(EventItem ei) {
 		EntityManager em = emfactory.createEntityManager();
 		em.getTransaction().begin();
 		em.persist(ei);
 		em.getTransaction().commit();
-		em.close();	
+		em.close();
 	}
-	
-	public List<EventItem> showAllItems(){
+
+	public List<EventItem> showAllItems() {
 		EntityManager em = emfactory.createEntityManager();
 		List<EventItem> allItems = em.createQuery("SELECT i from EventItem i").getResultList();
 		return allItems;
 	}
-	
+
 	public void deleteItem(EventItem toDelete) {
 		EntityManager em = emfactory.createEntityManager();
 		em.getTransaction().begin();
 		TypedQuery<EventItem> typedQuery = em.createQuery("select ei from EventItem ei where "
-				//+ "ei.eventDate = :selectedEventDate and "
-				+ "ei.eventType = :selectedEventType and "
-				+ "ei.eventName = :selectedEventName", EventItem.class);
-		
-		//typedQuery.setParameter("selectedEventDate", toDelete.getEventDate());
+				// + "ei.eventDate = :selectedEventDate and "
+				+ "ei.eventType = :selectedEventType and " + "ei.eventName = :selectedEventName", EventItem.class);
+
+		// typedQuery.setParameter("selectedEventDate", toDelete.getEventDate());
 		typedQuery.setParameter("selectedEventType", toDelete.getEventType());
 		typedQuery.setParameter("selectedEventName", toDelete.getEventName());
-		
+
 		typedQuery.setMaxResults(1);
-		
+
 		EventItem result = typedQuery.getSingleResult();
-		
+
 		em.remove(result);
 		em.getTransaction().commit();
 		em.close();
@@ -59,7 +55,7 @@ public class EventItemHelper {
 		// TODO Auto-generated method stub
 		EntityManager em = emfactory.createEntityManager();
 		em.getTransaction().begin();
-		
+
 		EventItem found = em.find(EventItem.class, idToEdit);
 		em.close();
 		return found;
@@ -69,7 +65,7 @@ public class EventItemHelper {
 		// TODO Auto-generated method stub
 		EntityManager em = emfactory.createEntityManager();
 		em.getTransaction().begin();
-		
+
 		em.merge(toEdit);
 		em.getTransaction().commit();
 		em.close();
@@ -79,10 +75,11 @@ public class EventItemHelper {
 		// TODO Auto-generated method stub
 		EntityManager em = emfactory.createEntityManager();
 		em.getTransaction().begin();
-		TypedQuery<EventItem> typedQuery = em.createQuery("select ei from EventItem ei where ei.eventType = :selectedEventType", EventItem.class);
-		
+		TypedQuery<EventItem> typedQuery = em
+				.createQuery("select ei from EventItem ei where ei.eventType = :selectedEventType", EventItem.class);
+
 		typedQuery.setParameter("selectedEventType", eventType);
-		
+
 		List<EventItem> foundItems = typedQuery.getResultList();
 		em.close();
 		return foundItems;
@@ -92,10 +89,11 @@ public class EventItemHelper {
 		// TODO Auto-generated method stub
 		EntityManager em = emfactory.createEntityManager();
 		em.getTransaction().begin();
-		TypedQuery<EventItem> typedQuery = em.createQuery("select ei from EventItem ei where ei.eventName = :selectedEventName", EventItem.class);
-		
+		TypedQuery<EventItem> typedQuery = em
+				.createQuery("select ei from EventItem ei where ei.eventName = :selectedEventName", EventItem.class);
+
 		typedQuery.setParameter("selectedEventName", eventName);
-		
+
 		List<EventItem> foundItems = typedQuery.getResultList();
 		em.close();
 		return foundItems;
@@ -105,15 +103,16 @@ public class EventItemHelper {
 		// TODO Auto-generated method stub
 		EntityManager em = emfactory.createEntityManager();
 		em.getTransaction().begin();
-		TypedQuery<EventItem> typedQuery = em.createQuery("select ei from EventItem ei where ei.eventDate = :selectedEventDate", EventItem.class);
-		
+		TypedQuery<EventItem> typedQuery = em
+				.createQuery("select ei from EventItem ei where ei.eventDate = :selectedEventDate", EventItem.class);
+
 		typedQuery.setParameter("selectedEventDate", eventDate);
-		
+
 		List<EventItem> foundItems = typedQuery.getResultList();
 		em.close();
 		return foundItems;
 	}
-	
+
 	public void cleanUp() {
 		emfactory.close();
 	}
